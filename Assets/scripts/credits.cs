95e752181045165034867582bc2058614dab51d5
DeaMladic <niyazovamariya@gmail.com>
2018-09-21 14:15:52 +0600

    удаление архива

---
+++ Assets/scripts/credits.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class credits : MonoBehaviour {
+
+	public void StartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
